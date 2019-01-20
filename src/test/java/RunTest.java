a98a48c457a16b96385e9465d8b8d57b8da78378
Karthigayan Devan <karthigayan_devan@homedepot.com>
2019-01-19 21:12:04 -0500

    add unit tests

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -3,7 +3,27 @@ import static org.junit.Assert.*;
 
 public class RunTest {
   @Test
-  public void RunTest() {
+  public void RunTest1() {
+      assertEquals(1, 1);
+  }
+
+  @Test
+  public void RunTest2() {
+      assertEquals(1, 1);
+  }
+
+  @Test
+  public void RunTest3() {
+      assertEquals(1, 1);
+  }
+
+  @Test
+  public void RunTest4() {
+      assertEquals(1, 1);
+  }
+
+  @Test
+  public void RunTest5() {
       assertEquals(1, 1);
   }
 }
